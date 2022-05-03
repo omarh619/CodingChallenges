d32b7f4e7625b132c4585478d713d81641799744
Omar Hernandez <you@example.com>
2022-05-03 11:43:17 -0700

    This is a new commit for what I originally planned to be an amendmend

---
+++ 5-2-2022/code.js
@@ -0,0 +1,11 @@
+/* Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6] */
+function maps(x){
+    //let newArr = x.map(element=> element * 2);
+    //return newArr
+   return x.map(e => e * 2)
+}
+maps([1,2.3]) 
