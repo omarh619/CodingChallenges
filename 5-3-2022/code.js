e71bb6ecbef774790e324a79607bb2c138d3c0a9
Omar Hernandez <you@example.com>
2022-05-03 12:15:25 -0700

    Coding challenge and solution for 5-3-2022

---
+++ 5-3-2022/code.js
@@ -0,0 +1,7 @@
+//remove the spaces from the string, then return the resultant string.
+function noSpace(x){
+    //let newArr = x.replace(/\s+/g, '')
+    //return newArr
+    return x.replace(/\s+/g,  "")
+  
+  }
