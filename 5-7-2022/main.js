b64c197d2370ef862502c0ca51e8124270a85d95
Omar Hernandez <omarh619@yahoo.com>
2022-05-07 17:11:21 -0700

    coding challenge and solution for 5-7-2022

---
+++ 5-7-2022/main.js
@@ -0,0 +1,8 @@
+/* Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number. */
+
+function sumMix(x){
+    let sum = x.reduce((a,b)=> +b + a, 0);
+     return sum
+     }
