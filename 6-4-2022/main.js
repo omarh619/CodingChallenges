d6a7b5054724739ee9bf570be893b29dd0b36c92
Omar Hernandez <omarh619@yahoo.com>
2022-06-04 11:38:29 -0700

    coding challenge and solution for 6-4-2022

---
+++ 6-4-2022/main.js
@@ -0,0 +1,40 @@
+/* A square of squares
+You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+Task
+Given an integral number, determine if it's a square number:
+
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false */
+
+//P: num, tes will always be an integrsl number
+//R: return true if its a perfect square or else false
+    //conditional, if sqrt return true
+    function isItASquare(num){
+        if(Math.sqrt(num) % 1 == 0){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    // remainder 1 to equal 0 for a perfect square
+    //return true or false
+
+
+
+//E:
+isItASquare(4)// return true
+isItASquare(3)//false
+isItASquare(25)//true
