89540bbda52fe40955d7e462ba2865e1bfb906c7
Omar Hernandez <omarh619@yahoo.com>
2022-07-11 02:16:10 -0700

    coding challenge for 7-11-2022

---
+++ 7-11-2022/main.js
@@ -0,0 +1,30 @@
+/* For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!"
+
+Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+
+Note: Make sure you return the correct message with correct spaces and punctuation.
+
+Please leave feedback for this kata. Cheers!
+
+ */
+
+//P: (x) if input is a string return zach ate the cookie. If the input is an integer return Monica. anything else return the dog.
+//R: return who ate the cookie by determining the type of input 
+//E: cookie("Ryan") => "Who ate the last cookie? It was Zach!")
+    //cookie(26) => "Who ate the last cookie? It was Monica!"
+    //cookie(true) => "Who ate the last cookie? It was the dog!"
+
+    function whoAteTheCookie(x){
+        //write conditional if type is string
+        if(typeof x === 'string'){
+            return `Who ate the last cookie? It was Zach!`
+             //write conditional if type of is a number
+          }else if(typeof x === 'number'){
+            return `Who ate the last cookie? It was Monica!`
+             //write conditional for everything else
+          }else{
+            return "Who ate the last cookie? It was the dog!"
+          }
+        
+       
+    }
