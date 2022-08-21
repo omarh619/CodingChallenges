369f0b1658a961c2d1d38f26bb0f7c4cc21172e8
Omar Hernandez <omarh619@yahoo.com>
2022-08-21 12:11:29 -0700

    code challenge and solution for 8-21-2022

---
+++ 8-21-2022/main.js
@@ -0,0 +1,16 @@
+/* Return the type of the sum of the two arguments
+
+ */
+
+//P: (a,b) get the sum, return the type of the sum,
+//R: Return the type of the sum of the two arguments
+//E:
+/*  (12, 1), 'number');
+    ('d', 1), 'string');
+    (1, 'a'), 'string') */
+
+    function typeOfSum(a, b) {
+        // find the sum
+        // return typeof
+        return typeof(a+b)
+        }
