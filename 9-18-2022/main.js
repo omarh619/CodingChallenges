943308f9a8330bd3fce704b7f3e84f6a2f32c67d
Omar Hernandez <omarh619@yahoo.com>
2022-09-18 12:15:21 -0700

    ARRAYS code challenge and solution for 9-18-2022

---
+++ 9-18-2022/main.js
@@ -0,0 +1,23 @@
+/* We want an array, but not just any old array, an array with contents!
+
+Write a function that produces an array with the numbers 0 to N-1 in it.
+
+For example, the following code will result in an array containing the numbers 0 to 4:
+
+arr(5) // => [0,1,2,3,4]
+Note: The parameter is optional. So you have to give it a default value. */
+
+// P: (numbers), Write a function that produces an array with the numbers 0 to N-1 in it.
+//R: return an array with the numbers 0 to n-1
+//E: numbers(5) // => [0,1,2,3,4]
+
+function arr(n){
+    // create variable to hold new array
+    var newArr = [];
+    // loop through and push numbers to array
+    for(var i = 0; i < n; i++){
+      newArr.push(i);
+    }
+    //return array
+    return newArr;
+  }
