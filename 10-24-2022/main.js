015dd86ca8f0b1b82d510df702450be2faaa0bd2
Omar Hernandez <omarh619@yahoo.com>
2022-10-24 20:10:30 -0700

    code challenge and solution for 10-24-2022

---
+++ 10-24-2022/main.js
@@ -0,0 +1,24 @@
+/* In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc. */
+
+// replace letter with numb3r 1-26, if not a letter ignore it
+
+//alphabetPosition("The sunset sets at twelve o' clock.")
+//Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+ 
+function alphabetPosition(text) {
+    //create variable that holds alph => split
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    return text.toLowerCase().split('')
+    //filter => map =>join
+    .filter( letter => {
+      let index = alphabet.indexOf(letter);
+      return index > -1;
+    }  )
+    .map( letter => alphabet.indexOf(letter) + 1 )
+    .join(' ')
+  }
